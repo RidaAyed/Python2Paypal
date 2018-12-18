6ce879bb1a75c808a79f5b0b5b9e6ada69829b37
Bernard Halas <bernard.halas@berops.com>
2018-12-18 15:57:24 +0100

    Add test for loading secrets and oauth workflow

--- tests/test_tsv_automation.py
+++ tests/test_tsv_automation.py
@@ -6,7 +6,13 @@ Test suite for tsv_automation.py
 
 from src import tsv_automation as src
 import json
+import oauthlib
+import os
 import pytest
+import vcr
+
+def cmp_dict(dict1, dict2):
+  return json.dumps(dict1, sort_keys = True) == json.dumps(dict2, sort_keys = True)
 
 def assert_complain(capsys, complain_msg, method, *args):
   with pytest.raises(SystemExit) as module_exit:
@@ -44,7 +50,29 @@ def test_validate_params():
   expected_params = {'--from-date': '2016-07-01T00:00:00',
                     '--help': False,
                     '--to-date': '2018-07-02T23:59:59'}
-  assert json.dumps(src.validate_params(params), sort_keys = True) == json.dumps(expected_params, sort_keys = True)
+  assert cmp_dict(src.validate_params(params), expected_params)
+
+def test_load_secrets(capsys):
+  expected = {
+    'client_id': os.environ['CLIENT_ID'],
+    'client_secret': os.environ['CLIENT_SECRET']
+  }
+
+  result = src.load_secrets()
+  assert cmp_dict(expected, result)
+
+  result = src.load_secrets(None)
+  assert cmp_dict(expected, result)
+
+  expected = {
+    'client_id': 'test_id',
+    'client_secret': 'test_secret'
+  }
+  result = src.load_secrets("test_secrets.yml")
+  assert cmp_dict(expected, result)
+
+  complain_msg = "File not found: missing_file.yml"
+  assert_complain(capsys, complain_msg, src.load_secrets, "missing_file.yml")
 
 def test_check_status_code(capsys):
   code = 200
@@ -53,3 +81,16 @@ def test_check_status_code(capsys):
   code = 201
   complain_msg = "Received unexpected status code " + str(code) + " != 200"
   assert_complain(capsys, complain_msg, src.check_status_code, code)
+
+@vcr.use_cassette('vcr_cassettes/auth_secrets.yaml')
+def test_obtain_auth_secrets(capsys):
+  secrets = src.load_secrets()
+  token = src.obtain_auth_token(secrets)
+  assert type(token) is oauthlib.oauth2.rfc6749.tokens.OAuth2Token
+
+  secrets = {
+    'client_id': 'broken_id',
+    'client_secret': 'broken_secret'
+  }
+  token = src.obtain_auth_token(secrets)
+  assert type(token) is oauthlib.oauth2.rfc6749.tokens.OAuth2Token
